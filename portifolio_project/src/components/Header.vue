d5e8464655ffdaa96d8fe02aee643f1dacb3b0e7
Guilherme <guilhermesimoes934@gmail.com>
2025-09-27 21:19:11 -0300

    ajustando responsividade para celular

--- portifolio_project/src/components/Header.vue
+++ portifolio_project/src/components/Header.vue
@@ -2,11 +2,14 @@
   <header>
     <nav>
       <a href="#home" class="brand">Meu Portfólio</a>
-      <ul>
-        <li><a href="#about">Sobre</a></li>
-        <li><a href="#projects">Projetos</a></li>
-        <li><a href="#skills">Habilidades</a></li>
-        <li><a href="#contact">Contato</a></li>
+      <div class="menu-toggle" @click="toggleMenu">
+        <i class="fas fa-bars"></i>
+      </div>
+      <ul :class="{ 'nav-active': isMenuOpen }">
+        <li><a href="#about" @click="closeMenu">Sobre</a></li>
+        <li><a href="#projects" @click="closeMenu">Projetos</a></li>
+        <li><a href="#skills" @click="closeMenu">Habilidades</a></li>
+        <li><a href="#contact" @click="closeMenu">Contato</a></li>
         <li>
           <button @click="toggleTheme" class="theme-switcher">
             <i v-if="theme === 'dark'" class="fas fa-sun"></i>
@@ -22,9 +25,20 @@
 export default {
   name: 'Header',
   props: ['theme'],
+  data() {
+    return {
+      isMenuOpen: false
+    }
+  },
   methods: {
     toggleTheme() {
       this.$emit('toggle-theme');
+    },
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeMenu() {
+      this.isMenuOpen = false;
     }
   }
 }
